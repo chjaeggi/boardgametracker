fe5b80e8d153d9d35b8cb5d9ed9466cdbbd057cc
Christian Jaeggi <chr.jaeggi@gmail.com>
2018-12-29 20:48:05 +0100

    Fix navigation
    + Remove home option from drawer
    + Adjust short name

--- app/src/main/java/com/chjaeggi/boardgametracker/mobile/MainActivity.kt
+++ app/src/main/java/com/chjaeggi/boardgametracker/mobile/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.chjaeggi.boardgametracker.mobile
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
@@ -8,6 +9,7 @@ import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.chjaeggi.boardgametracker.R
 import com.chjaeggi.boardgametracker.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,11 +23,15 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
         NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
-
         binding.navigationView.setupWithNavController(navController)
 
         binding.setLifecycleOwner(this)
+
+//        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+//            Timber.d("XXX: $destination")
+//        }
     }
+
 
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(
@@ -33,4 +39,22 @@ class MainActivity : AppCompatActivity() {
             binding.drawerLayout
         )
     }
+
+//    override fun onSupportNavigateUp()
+//            = findNavController(R.id.main_nav_fragment).navigateUp()
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return when (item.itemId) {
+//            android.R.id.home -> {
+//                return NavigationUI.navigateUp(
+//                    Navigation.findNavController(
+//                        this,
+//                        R.id.main_nav_fragment
+//                    ), binding.drawerLayout
+//                )
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
+
 }
