6f5c613612d4acce3d8231ff9536fb9246b51ccc
Christian Jaeggi <chr.jaeggi@gmail.com>
2018-12-31 07:59:12 +0100

    Adjust navigation
    + Refactor (move) classes and packages
    + Add DetailsActivity including a boilerplate layout
    + Adjust navigation through the app
    + Implement a basic details button that opens a new activity

--- app/src/main/java/com/chjaeggi/boardgametracker/mobile/home/overview/OverviewFragment.kt
+++ app/src/main/java/com/chjaeggi/boardgametracker/mobile/home/overview/OverviewFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.chjaeggi.boardgametracker.R
 import com.chjaeggi.boardgametracker.databinding.FragmentOverviewBinding
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -19,6 +20,11 @@ class OverviewFragment : androidx.fragment.app.Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_overview, container, false)
+
+        binding.detailsButton.setOnClickListener  { view ->
+            view.findNavController().navigate(R.id.details_activity)
+        }
+
         binding.setLifecycleOwner(this)
         return binding.root
     }
