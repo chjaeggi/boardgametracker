ea34eb08f5eb42933e575302b4217d60378dbf25
Christian Jaeggi <chr.jaeggi@gmail.com>
2019-01-01 21:42:11 +0100

    Add RecyclerView stub
    + Change fonts
    + Adjust icons
    + Add sample text
    + Add a stub RecyclerView to the overview fragment
    + Add a games-list-view stub (xml) …
    + and the appropriate Adapter and ViewHolder for the games items overview.
    + Also add a data class representing the fields to show in a single item.

--- app/src/main/java/com/chjaeggi/boardgametracker/mobile/home/overview/OverviewFragment.kt
+++ app/src/main/java/com/chjaeggi/boardgametracker/mobile/home/overview/OverviewFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
 import com.chjaeggi.boardgametracker.R
 import com.chjaeggi.boardgametracker.databinding.FragmentOverviewBinding
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -15,16 +14,14 @@ class OverviewFragment : androidx.fragment.app.Fragment() {
     private val viewModel by viewModel<OverviewViewModel>()
     private lateinit var binding: FragmentOverviewBinding
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_overview, container, false)
 
-        binding.detailsButton.setOnClickListener  { view ->
-            view.findNavController().navigate(R.id.details_activity)
-        }
-
+        binding.allGames.adapter = GamesAdapter(viewModel.games)
         binding.setLifecycleOwner(this)
         return binding.root
     }
