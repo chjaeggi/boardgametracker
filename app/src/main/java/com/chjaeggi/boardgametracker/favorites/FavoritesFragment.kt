e46c0a799e155f280bd14fbf3cd78395305fb429
Christian Jaeggi <chr.jaeggi@gmail.com>
2020-02-10 06:57:11 +0100

    make search in UI available

--- app/src/main/java/com/chjaeggi/boardgametracker/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/chjaeggi/boardgametracker/favorites/FavoritesFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.chjaeggi.boardgametracker.R
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.input.input
 import com.chjaeggi.boardgametracker.databinding.FragmentFavoritesBinding
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -20,6 +22,13 @@ class FavoritesFragment : androidx.fragment.app.Fragment() {
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_favorites, container, false)
         binding.lifecycleOwner = this
+
+        binding.addGame.setOnClickListener { view ->
+            MaterialDialog(requireContext()).show {
+                input()
+                positiveButton(R.string.search)
+            }
+        }
         return binding.root
     }
 }
--- app/src/main/java/com/chjaeggi/boardgametracker/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/chjaeggi/boardgametracker/favorites/FavoritesViewModel.kt
@@ -1,7 +1,17 @@
 package com.chjaeggi.boardgametracker.favorites
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.chjaeggi.boardgametracker.util.SingleLiveEvent
+import timber.log.Timber
 
 class FavoritesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _addGame = SingleLiveEvent<String>()
+    val addGame: LiveData<String> = _addGame
+
+    fun addGameClicked(game: String) {
+        Timber.d("game = $game")
+    }
+
 }
